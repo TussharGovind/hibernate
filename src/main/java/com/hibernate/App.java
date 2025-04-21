9caa8439d4490dee63ef8d8f25e80d03340a138b
cold_comets <tusshargovind@gmail.com>
2025-04-21 14:39:53 +0530

    update db

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -2,7 +2,9 @@ package com.hibernate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
 
 /**
  * Hello world!
@@ -22,8 +24,10 @@ public class App
         cfg.configure();
         SessionFactory sf = cfg.buildSessionFactory();
         Session session = sf.openSession();
-
+        Transaction tx = session.beginTransaction();
         session.save(s1);
+        tx.commit();
+        
         System.out.println(s1);
     }
 }
